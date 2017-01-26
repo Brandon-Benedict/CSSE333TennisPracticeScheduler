e83d959aa7c52d994fd1744e437bc13cf50120ba
Brandon-Benedict <benedib1@rose-hulman.edu>
2017-01-25 23:40:01 -0500

    added table and c# method get actor

---
+++ TennisPracticeScheduler/Controllers/TeamController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TennisPracticeScheduler.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        public ActionResult GetPlayers(String name)
+        {
+            TennisSchedulerDBDataContext DBcontext = new TennisSchedulerDBDataContext();
+            var player = DBcontext.Players.Where(a => a.FirstName.Contains(name));
+            return Json(player, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
